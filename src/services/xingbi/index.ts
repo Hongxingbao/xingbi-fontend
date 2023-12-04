30cf4314760127877cb4b1852e9b443e3d16a592
24840 <2484047829@qq.com>
2023-12-04 21:08:17 +0800

    初步完成登录注册功能

---
+++ src/services/xingbi/index.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+/* eslint-disable */
+// API 更新时间：
+// API 唯一标识：
+import * as chartController from './chartController';
+import * as fileController from './fileController';
+import * as userController from './userController';
+export default {
+  chartController,
+  fileController,
+  userController,
+};
